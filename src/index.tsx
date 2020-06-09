caecc2a6687e0f60a8867506478d0cd526c7aa7b
pablopunk <yo@pablo.pink>
2020-06-09 11:32:43 +0200

    Simpler types

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import Router from 'next/router'
 
-interface IOptions {
-  asUrl: string
-  statusCode: number
-}
-
-export default (redirectUrl: string, options: IOptions) =>
+export default (
+  redirectUrl: string,
+  options?: { asUrl: string; statusCode: number }
+) =>
   class extends React.Component {
     static async getInitialProps({ res }) {
       if (res) {
-        res.writeHead(options.statusCode ?? 301, { Location: redirectUrl })
+        res.writeHead(options?.statusCode ?? 301, { Location: redirectUrl })
         res.end()
       } else {
-        if (options.asUrl != null) {
+        if (options?.asUrl != null) {
           Router.push(redirectUrl, options.asUrl, { shallow: true })
         } else {
           Router.push(redirectUrl)
